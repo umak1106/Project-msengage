386db04ffa4e21e244dd9ac2008f2d0a96378958
Uma Kadam <90750049+umak1106@users.noreply.github.com>
2022-05-24 01:59:55 +0530

    Add files via upload

---
+++ Health++/app.py
@@ -0,0 +1,22 @@
+#Library imports
+import numpy as np
+import streamlit as st
+from keras.models import load_model
+import app5
+import app2
+import app3
+import app4
+import app1
+import app6
+PAGES = {
+    "Mask Detection": app5 ,
+    "Mental Health": app2 ,
+    "Health" : app3 , 
+    "Covid19": app4 ,
+    "Plant_Disease" :app1 ,
+    "Cancer Disease":app6
+}
+st.sidebar.title('Health++ ')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
